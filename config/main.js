24bbbd0aade6d6a652b6d3c32f2d8c0149f2fb64
AMNB4 <118478433+AMNB4@users.noreply.github.com>
2025-01-07 14:34:11 -0800

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "CyberGames"; // Change this to change the name of your website.
+var subtext = "DONTSHARE"; // set the subtext
 
 // more settings in main.css
 
